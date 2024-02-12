54f68f5600f12d1343ab1872627a9a0dadc98312
Alexander Drapun <dr.sasha2602@mail.ru>
2024-02-13 02:48:40 +0300

    the js files were put in order

--- wwwroot/js/Main.js
+++ wwwroot/js/Main.js
@@ -1,14 +1,8 @@
-﻿
-$(document).ready(function () {
-    $('table').DataTable({
-        "language": {
-            "lengthMenu": document.querySelector('#LengthMenu').textContent,
-            "zeroRecords": document.querySelector('#ZeroRecords').textContent,
-            "info": document.querySelector('#Info').textContent,
-            "infoEmpty": document.querySelector('#InfoEmpty').textContent,
-            "infoFiltered": document.querySelector('#InfoFiltered').textContent,
-            "search": document.querySelector('#Search').textContent
-        }
+﻿$(document).ready(function () {
+    new DataTable('Table', {
+        lengthMenu: [
+            [10, 12]
+        ]
     });
 });
 
